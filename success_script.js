c83f7378f232b61c16ffae786c4103c53f0a6961
Prajwal Pandit <88018651+prajwal0909@users.noreply.github.com>
2024-11-25 12:52:24 +0530

    Add files via upload

---
+++ success_script.js
@@ -0,0 +1,3 @@
+document.getElementById("redirect-btn").addEventListener("click", function() {
+    window.location.href = "intent://instagram.com/_n/mainfeed?ig_mid=40F05BBA-E130-4A21-8FFC-457322C737B8&utm_source=instagramweb#Intent;package=com.instagram.android;action=android.intent.action.VIEW;scheme=https;S.browser_fallback_url=https%3A%2F%2Fplay.google.com%2Fstore%2Fapps%2Fdetails%3Fid%3Dcom.instagram.android%26referrer%3Dig_mid%253D40F05BBA-E130-4A21-8FFC-457322C737B8%2526utm_campaign%253DrootLandingPage%2526utm_content%253Dlo%2526utm_source%253Dinstagramweb%2526utm_medium%253DinstallLink;end";
+});
